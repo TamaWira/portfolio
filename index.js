43b826f60b30d94fbeb7353ef68b9ec0085f7eba
TamaWira <asusrog20012001@gmail.com>
2024-02-18 21:18:53 +0800

    feat: initialize project

---
+++ index.js
@@ -0,0 +1,18 @@
+function myFunction() {
+  console.log(document.getElementById("myDropdown"));
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    console.log(dropdowns);
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
